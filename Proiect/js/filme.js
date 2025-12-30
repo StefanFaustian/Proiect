5dcd755c83f3a927b1e9345eb9cd7de727d483ef
StefanFaustian <stefanpetcovici@gmail.com>
2025-12-30 04:02:25 +0200

    Finalizare pagina filme

--- Proiect/js/filme.js
+++ Proiect/js/filme.js
@@ -1,131 +1,199 @@
-const filme = [
-    {
-        id: "film1",
-        titlu: "O viață minunată",
-        poster: "resurse/imagini/filme/wonderful.png",
-        an: 1946,
-        imdb: 8.6,
-        sinopsis: "",
-        regizor: "",
-        distributie: "",
-        trailer: ""
-    },
+// const filme = [
+//     {
+//         id: "film1",
+//         titlu: "O viață minunată",
+//         poster: "resurse/imagini/filme/wonderful.png",
+//         an: 1946,
+//         imdb: 8.6,
+//         sinopsis: "Un înger este trimis din Ceruri pentru a ajuta un om de afaceri extrem de frustrat să vadă valoarea propriei vieți.",
+//         regizor: "Frank Capra",
+//         distributie: "James Stewart, Donna Reed, Lionel Barrymore, Thomas Mitchell",
+//         trailer: "https://www.youtube.com/watch?v=iLR3gZrU2Xo"
+//     },
 
-    {
-        id: "film2",
-        titlu: "Greu de ucis",
-        poster: "resurse/imagini/filme/diehard.png",
-        an: 1988,
-        imdb: 8.6,
-        sinopsis: "",
-        regizor: "",
-        distributie: "",
-        trailer: ""
-    },
+//     {
+//         id: "film2",
+//         titlu: "Greu de ucis",
+//         poster: "resurse/imagini/filme/diehard.png",
+//         an: 1988,
+//         imdb: 8.6,
+//         sinopsis: "Un polițist din New York, John McClane, încearcă să-și salveze soția înstrăinată și alte câteva persoane luate ostatice de teroriști în timpul unei petreceri de Ajunul Crăciunului la zgârie-norii Nakatomi Plaza din Los Angeles, California.",
+//         regizor: "John McTiernan",
+//         distributie: "Bruce Willis, Alan Rickman, Bonnie Bedelia, Reginald VelJohnson",
+//         trailer: "https://www.youtube.com/watch?v=jaJuwKCmJbY"
+//     },
 
-    {
-        id: "film3",
-        titlu: "Singur acasă",
-        poster: "resurse/imagini/filme/homealone.png",
-        an: 1990,
-        imdb: 7.7,
-        sinopsis: "",
-        regizor: "",
-        distributie: "",
-        trailer: ""
-    },
+//     {
+//         id: "film3",
+//         titlu: "Singur acasă",
+//         poster: "resurse/imagini/filme/homealone.png",
+//         an: 1990,
+//         imdb: 7.7,
+//         sinopsis: "Un copil scandalagiu în vârstă de opt ani, lăsat din greșeală singur acasă, trebuie să-și apere casa împotriva a doi hoți în Ajunul Crăciunului.",
+//         regizor: "Chris Columbus",
+//         distributie: "Macaulay Culkin, Joe Pesci, Daniel Stern",
+//         trailer: "https://www.youtube.com/watch?v=jEDaVHmw7r4"
+//     },
 
-    {
-        id: "film4",
-        titlu: "Cum a furat Grinch Crăciunul",
-        poster: "resurse/imagini/filme/grinch1.png",
-        an: 2000,
-        imdb: 6.4,
-        sinopsis: "",
-        regizor: "",
-        distributie: "",
-        trailer: ""
-    },
+//     {
+//         id: "film4",
+//         titlu: "Cum a furat Grinch Crăciunul",
+//         poster: "resurse/imagini/filme/grinch1.png",
+//         an: 2000,
+//         imdb: 6.4,
+//         sinopsis: "La marginea orașului Whoville trăiește un Grinch verde, în căutare de răzbunare, care plănuiește să strice Crăciunul tuturor cetățenilor orașului.",
+//         regizor: "Ron Howard",
+//         distributie: "Jim Carrey, Taylor Momsen, Jeffrey Tambor",
+//         trailer: "https://www.youtube.com/watch?v=DD0m9t4WHEQ"
+//     },
 
-    {
-        id: "film5",
-        titlu: "The Holdovers",
-        poster: "resurse/imagini/filme/holdovers.png",
-        an: 2023,
-        imdb: 7.9,
-        sinopsis: "",
-        regizor: "",
-        distributie: "",
-        trailer: ""
-    },
+//     {
+//         id: "film5",
+//         titlu: "The Holdovers",
+//         poster: "resurse/imagini/filme/holdovers.png",
+//         an: 2023,
+//         imdb: 7.9,
+//         sinopsis: "În 1970, un profesor de istorie morocănos de la un internat din New England rămâne în campus în timpul vacanței de Crăciun pentru a supraveghea elevii și ajunge să formeze o legătură neobișnuită cu un scandalagiu inteligent, dar afectat.",
+//         regizor: "Alexander Payne",
+//         distributie: "Paul Giamatti, Da'Vine Joy Randolph, Dominic Sessa, Carrie Preston",
+//         trailer: "https://www.youtube.com/watch?v=AhKLpJmHhIg"
+//     },
 
-    {
-        id: "film6",
-        titlu: "Singur acasă 2 - Pierdut în New York",
-        poster: "resurse/imagini/filme/homealone2.png",
-        an: 1992,
-        imdb: 6.9,
-        sinopsis: "",
-        regizor: "",
-        distributie: "",
-        trailer: ""
-    },
+//     {
+//         id: "film6",
+//         titlu: "Singur acasă 2 - Pierdut în New York",
+//         poster: "resurse/imagini/filme/homealone2.png",
+//         an: 1992,
+//         imdb: 6.9,
+//         sinopsis: "Kevin este din nou separat de familia sa când se îmbarcă accidental într-un zbor spre New York în timpul unei călătorii de Crăciun la Miami. Cu toate acestea, se întâlnește cu aceiași spărgători, care acum plănuiesc să jefuiască un magazin de jucării în Ajunul Crăciunului.",
+//         regizor: "Chris Columbus",
+//         distributie: "Macaulay Culkin, Joe Pesci, Daniel Stern",
+//         trailer: "https://www.youtube.com/watch?v=5h9VDUNtoto"
+//     },
 
-    {
-        id: "film7",
-        titlu: "Expresul polar",
-        poster: "resurse/imagini/filme/polarexpress.png",
-        an: 2004,
-        imdb: 6.6,
-        sinopsis: "",
-        regizor: "",
-        distributie: "",
-        trailer: ""
-    },
+//     {
+//         id: "film7",
+//         titlu: "Expresul polar",
+//         poster: "resurse/imagini/filme/polarexpress.png",
+//         an: 2004,
+//         imdb: 6.6,
+//         sinopsis: "În Ajunul Crăciunului, un băiețel pornește într-o aventură magică spre Polul Nord la bordul Polar Express, în timp ce învață despre prietenie, curaj și spiritul Crăciunului.",
+//         regizor: "Robert Zemeckis",
+//         distributie: "Tom Hanks, Chris Coppola, Michael Jeter",
+//         trailer: "https://www.youtube.com/watch?v=f0oyHtZiJJs"
+//     },
 
-    {
-        id: "film8",
-        titlu: "Armă mortală",
-        poster: "resurse/imagini/filme/lethalweapons.png",
-        an: 1987,
-        imdb: 7.6,
-        sinopsis: "",
-        regizor: "",
-        distributie: "",
-        trailer: ""
-    },
+//     {
+//         id: "film8",
+//         titlu: "Armă mortală",
+//         poster: "resurse/imagini/filme/lethalweapons.png",
+//         an: 1987,
+//         imdb: 7.6,
+//         sinopsis: "Doi polițiști recent împerecheați, complet opuși, trebuie să-și lase deoparte diferențele pentru a prinde o bandă de traficanți de droguri.",
+//         regizor: "Richard Donner",
+//         distributie: "Mel Gibson, Danny Glover, Gary Busey",
+//         trailer: "https://www.youtube.com/watch?v=RLu49mRcmYs"
+//     },
 
-    {
-        id: "film9",
-        titlu: "Cu ochii larg închiși",
-        poster: "resurse/imagini/filme/eyeswideshut.png",
-        an: 1999,
-        imdb: 7.5,
-        sinopsis: "",
-        regizor: "Stanley Kubrick",
-        distributie: "",
-        trailer: ""
-    },
+//     {
+//         id: "film9",
+//         titlu: "Cu ochii larg închiși",
+//         poster: "resurse/imagini/filme/eyeswideshut.png",
+//         an: 1999,
+//         imdb: 7.5,
+//         sinopsis: "Un doctor din Manhattan pornește într-o odisee bizară, care durează o noapte, după ce soția sa mărturisește că își dorește ceva neîmplinit.",
+//         regizor: "Stanley Kubrick",
+//         distributie: "Tom Cruise, Nicole Kidman, Todd Field",
+//         trailer: "https://www.youtube.com/watch?v=xgVo96JaqeM"
+//     },
 
-    {
-        id: "film10",
-        titlu: "Batman revine",
-        poster: "resurse/imagini/filme/batman.png",
-        an: 1992,
-        imdb: 7.1,
-        sinopsis: "",
-        regizor: "Tim Burton",
-        distributie: "",
-        trailer: ""
-    },
-];
+//     {
+//         id: "film10",
+//         titlu: "Batman revine",
+//         poster: "resurse/imagini/filme/batman.png",
+//         an: 1992,
+//         imdb: 7.1,
+//         sinopsis: "În timp ce Batman se confruntă cu un bărbat diform care se autointitulează Pinguinul și care face ravagii în Gotham cu ajutorul unui om de afaceri crud, o angajată a acestuia din urmă devine Femeia-Pisică și are propria ei vendetă.",
+//         regizor: "Tim Burton",
+//         distributie: "Michael Keaton, Danny DeVito, Michelle Pfeiffer, Christopher Walken",
+//         trailer: "https://www.youtube.com/watch?v=TlbtLfWvFbo"
+//     },
+// ];
 
+// fetch("json/filme.json").then(function(response) {
+//     if(response.status == 200)
+//         return response.json();
+//     else
+//         throw "Eroare";
+// }).then(function(filmeLocal) {
+//     filme = filmeLocal;
+//     for (let i = 0; i<filme.length; i++) {
+//         let filmCurent = document.getElementById(`${filme[i].id}`)
+//         let titluFilmCurent = filmCurent.querySelector("h6");
+//         filmCurent.style.backgroundImage = `url("${filme[i].poster}")`;
+//         titluFilmCurent.innerText = `${filme[i].titlu} (${filme[i].an})`
+//     }
+// }).catch(function(err) {
+//     alert(err);
+// })
 
-for (let i = 0; i<filme.length; i++) {
-    // let grid = document.getElementById("wrapper-grid");
-    // grid.appendChild("a")
-    let filmCurent = document.getElementById(`${filme[i].id}`)
-    let titluFilmCurent = filmCurent.querySelector("h6");
-    filmCurent.style.backgroundImage = `url("${filme[i].poster}")`;
-    titluFilmCurent.innerText = `${filme[i].titlu} (${filme[i].an})`
+let filme = {};
+fetch("json/filme.json").then(function(response) {
+    if(response.status == 200)
+        return response.json();
+    else
+        throw "Eroare";
+}).then(function(filmeLocal) {
+    filme = filmeLocal;
+    for (let id in filme) {
+        let filmCurent = document.getElementById(`${id}`)
+        let titluFilmCurent = filmCurent.querySelector("h6");
+        let film = filme[id];
+        filmCurent.style.backgroundImage = `url("${film.poster}")`;
+        titluFilmCurent.innerText = `${film.titlu} (${film.an})`;
+    }
+    initModal(filme);
+
+}).catch(function(err) {
+    alert(err);
+})
+
+function initModal(filme) {
+// for (let id in filme) {
+//     console.log(id);
+//     console.log(filme[id]);
+// }
+    
+    let modal = document.getElementById("film-modal");
+    let casete = document.getElementsByClassName("film-card");
+    let buton = document.querySelector(".modal-antet button");
+
+    buton.onclick = function() {
+        modal.classList.remove("modal-activ");
+        document.body.classList.remove("no-scroll");
+    }
+
+    for (let caseta of casete) {
+        caseta.onclick = function() {
+            window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
+            modal.classList.add("modal-activ");
+            document.body.classList.add("no-scroll");
+            const poster = document.querySelector(".poster");
+            const trailer = document.querySelector("iframe");
+            // const imdb = document.querySelector(".imdb");
+            // const titlu = document.querySelector(".titlu");
+            // const regizor = document.querySelector(".regizor");
+            // const distributie = document.querySelector(".distributie");
+            const film = filme[caseta.id];
+            poster.style.backgroundImage  = `url("${film.poster}")`;
+            trailer.src = film.trailer;
+            for (let detaliu in film) {
+                if (detaliu != "poster" && detaliu != "trailer") {
+                    document.querySelector(`.${detaliu}`).innerText = film[detaliu]
+                }
+            }
+    }
+    }
+
 }
+
